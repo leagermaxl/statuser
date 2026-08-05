dcf451d3bc8d31c2ce75b3b44fed8b362edab4c4
Max Lyubchenko <00eagermax00@gmail.com>
2026-08-05 16:25:34 +0200

    feat: config service & port

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,29 @@
+import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import Joi from 'joi';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-	imports: [],
+	imports: [
+		ConfigModule.forRoot({
+			isGlobal: true,
+			validationSchema: Joi.object({
+				PORT: Joi.number().default(3000),
+				NODE_ENV: Joi.string()
+					.valid('development', 'production', 'test')
+					.default('development'),
+
+				MEGAGROUP_LOGIN: Joi.string().required(),
+				MEGAGROUP_PASSWORD: Joi.string().required(),
+				MEGAGROUP_ADMIN_URL: Joi.string().uri().required(),
+
+				CDEK_WEBHOOK_SECRET: Joi.string().required(),
+			}),
+		}),
+		HttpModule,
+	],
 	controllers: [AppController],
 	providers: [AppService],
 })
